b411ceeebeadd43c31dcf9a4e05f726d3f104e3e
PriQue <39774455+prique@users.noreply.github.com>
2021-02-15 17:02:53 +0100

    Drop MiuiCam

--- common.mk
+++ common.mk
@@ -124,8 +124,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.full.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.full.xml \
     frameworks/native/data/etc/android.hardware.camera.raw.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.raw.xml
 
-$(call inherit-product, vendor/xiaomi/miuicamera/config.mk)
-
 # Display
 PRODUCT_PACKAGES += \
     disable_configstore \
